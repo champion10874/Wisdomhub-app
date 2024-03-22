bf3e01e5eeac7921606298e308abe3816ac23376
champion10874 <champion10875@gmail.com>
2024-03-22 15:04:59 +0200

    add auth unit test mock

---
+++ server/3-auth-service/src/controllers/test/mocks/auth.mock.ts
@@ -0,0 +1,50 @@
+import { Response } from 'express';
+import { IAuthDocument, IAuthPayload } from '@hassonor/wisdomhub-shared';
+
+export const authMockRequest = (sessionData: IJWT, body: IAuthMock, currentUser?: IAuthPayload | null, params?: unknown) => ({
+  session: sessionData,
+  body,
+  params,
+  currentUser
+});
+
+export const authMockResponse = (): Response => {
+  const res: Response = {} as Response;
+  res.status = jest.fn().mockReturnValue(res);
+  res.json = jest.fn().mockReturnValue(res);
+  return res;
+};
+
+export interface IJWT {
+  jwt?: string;
+}
+
+export interface IAuthMock {
+  id?: number;
+  username?: string;
+  email?: string;
+  password?: string;
+  createdAt?: Date | string;
+}
+
+export const authUserPayload: IAuthPayload = {
+  id: 1,
+  username: 'hasson_test',
+  email: 'hassontest@gmail.com',
+  iat: 1245282483
+};
+
+export const authMock: IAuthDocument = {
+  id: 1,
+  profilePublicId: '12542871373734565328',
+  username: 'hasson_test',
+  email: 'hassontest@gmail.com',
+  country: 'Israel',
+  profilePicture: '',
+  emailVerified: 1,
+  createdAt: '2024-01-01T07:43:23.342Z',
+  comparePassword: () => {
+  },
+  hashPassword: () => {
+  }
+} as unknown as IAuthDocument;
