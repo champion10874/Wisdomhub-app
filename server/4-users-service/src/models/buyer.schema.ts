e9cb03ac08dae3c47b0373f731e408cc85a9f74c
champion10874 <champion10875@gmail.com>
2024-04-06 18:44:42 +0300

    add buyer schema

---
+++ server/4-users-service/src/models/buyer.schema.ts
@@ -0,0 +1,20 @@
+import mongoose, { Model, model, Schema } from 'mongoose';
+import { IBuyerDocument } from '@hassonor/wisdomhub-shared';
+
+const buyerSchema: Schema = new Schema(
+  {
+    username: { type: String, required: true, index: true },
+    email: { type: String, required: true, index: true },
+    profilePicture: { type: String, required: true },
+    country: { type: String, required: true },
+    isSeller: { type: Boolean, default: false },
+    purchasedGigs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Gig' }],
+    createdAt: { type: Date }
+  },
+  {
+    versionKey: false
+  }
+);
+
+const BuyerModel: Model<IBuyerDocument> = model<IBuyerDocument>('Buyer', buyerSchema);
+export { BuyerModel };
