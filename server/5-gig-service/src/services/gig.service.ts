c4e94429780340c7a53d0e31736a3d2c5e87a627
champion10874 <champion10875@gmail.com>
2024-06-16 21:38:44 +0300

    add update gig rewview to gig.service

--- server/5-gig-service/src/services/gig.service.ts
+++ server/5-gig-service/src/services/gig.service.ts
@@ -1,4 +1,4 @@
-import { ISellerGig } from '@hassonor/wisdomhub-shared';
+import { IRatingTypes, IReviewMessageDetails, ISellerGig } from '@hassonor/wisdomhub-shared';
 import { addDataToIndex, deleteIndexData, getIndexedData, updateIndexedData } from '@gig/elasticsearch';
 import { gigsSearchBySellerId } from '@gig/services/search.service';
 import { GigModel } from '@gig/models/gig.schema';
@@ -101,4 +101,42 @@ const updateActiveGigProp = async (gigId: string, isGigActive: boolean): Promise
   return updatedGig;
 };
 
-export { getGigById, getSellerGigs, getSellerPausedGigs, createGig, deleteGig, updateGig, updateActiveGigProp };
+
+const updateGigReview = async (data: IReviewMessageDetails): Promise<void> => {
+  const ratingTypes: IRatingTypes = {
+    '1': 'one',
+    '2': 'two',
+    '3': 'three',
+    '4': 'four',
+    '5': 'five'
+  };
+  const ratingKey: string = ratingTypes[`${data.rating}`];
+  const gig = await GigModel.findOneAndUpdate(
+    { _id: data.gigId },
+    {
+      $inc: {
+        ratingsCount: 1, // Increment the total number of ratings by 1
+        ratingSum: data.rating, // Add the current rating to the total rating sum
+        [`ratingsCategories.${ratingKey}.value`]: data.rating, // Add the current rating to the corresponding rating category value
+        [`ratingsCategories.${ratingKey}.count`]: 1 // Increment the count of the corresponding rating category by 1
+      }
+    },
+    { new: true, upsert: true } // Return the updated document, and create it if it doesn't exist
+  ).exec();
+
+  if (gig) {
+    const data: ISellerGig = gig.toJSON?.() as ISellerGig;
+    await updateIndexedData('gigs', `${gig._id}`, data);
+  }
+};
+
+export {
+  getGigById,
+  getSellerGigs,
+  getSellerPausedGigs,
+  createGig,
+  deleteGig,
+  updateGig,
+  updateActiveGigProp,
+  updateGigReview
+};
